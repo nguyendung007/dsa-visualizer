cf0d9c69359f5b594486488dc4b614a009fc1ab6
Hakai_dugew <ndungclone007@gmail.com>
2026-06-16 20:51:16 +0700

    update vite config

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: true  // hoặc host: '0.0.0.0'
+    host: true,
+    proxy: {
+      '/api': {
+        target: 'http://localhost:8000',
+        changeOrigin: true,
+      }
+    }
   },
 })
