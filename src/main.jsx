8f999e6a6423fe1cdeb38216d435237924025a5b
Hakai_dugew <ndungclone007@gmail.com>
2026-06-16 20:49:09 +0700

    update main

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,17 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.jsx';
+import { AuthProvider } from './context/AuthContext.jsx';
+import { ProgressProvider } from './context/ProgressContext.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <AuthProvider>
+      <ProgressProvider> 
+        <App />
+      </ProgressProvider> 
+    </AuthProvider>
   </StrictMode>
 );
+
+
